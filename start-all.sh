1c8261ee38253509e8ab6d83f54cfc5ab651f946
Dan Manges <dan@rwx.com>
2021-12-27 15:52:21 -0500

    Disable durability for postgres

--- start-all.sh
+++ start-all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-bash -c 'cd ruby && docker-compose up --remove-orphans -d app'
-bash -c 'cd elixir && docker-compose up --remove-orphans -d app'
-bash -c 'cd go && docker-compose up --remove-orphans -d app'
-bash -c 'cd rust && docker-compose up --remove-orphans -d app'
+bash -c 'cd ruby && docker-compose up --remove-orphans -d db'
+bash -c 'cd elixir && docker-compose up --remove-orphans -d db'
+bash -c 'cd go && docker-compose up --remove-orphans - d db'
+bash -c 'cd rust && docker-compose up --remove-orphans -d db'
